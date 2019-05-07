ae15c71c6cde48d4045a47cfb18df58107c0b095
RhysRascal <50360296+RhysRascal@users.noreply.github.com>
2019-05-07 21:45:29 +0930

    Add files via upload
    Version 2

---
+++ Prototype/core/src/com/mygdx/game/Background.java
@@ -0,0 +1,17 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Background {
+
+    int width, height;
+    Texture image;
+
+
+    public Background(int width, int height, Texture image){
+        this.width = width;
+        this.height = height;
+        this.image = image;
+    }
+
+}
